b9c8ec4c256766efe5c8671890ebad968c62faf5
Ismael <youremail@yourdomain.com>
2024-10-15 12:39:39 +0200

    home hecho

--- web/public/javascripts/script.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelector("a#open-offcanvas").addEventListener("click",()=>{
-    document.querySelector("aside#offcanvas").classList.add("show");
-});
-
-document.querySelector("#offcanvas button.close").addEventListener("click",()=>{
-    document.querySelector("aside#offcanvas").classList.remove("show");
-});
